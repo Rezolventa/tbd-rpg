9b031e6c89cb5e3953dfcfa280f53f2b3b50b609
Rezolventa <rez0lventa@yandex.ru>
2022-06-06 01:58:06 +0300

    feat: raw architecture improvement i.e. action_manager, refactoring, focus image alpha

--- client.py
+++ client.py
@@ -1,41 +1,14 @@
-from datetime import datetime
-
 import pygame
 
-from network import NetworkManager
-from sprites import ScreenManager
+from sprites import GameController
 
 pygame.init()
 pygame.font.init()
 
-
-class Player:
-    def __init__(self):
-        self.client_connected_at = datetime.now()
-        self.time_count = None
-
-    @property
-    def get_time_count(self):
-        self.time_count = int((datetime.now() - self.client_connected_at).total_seconds())
-        return str(int((datetime.now() - self.client_connected_at).total_seconds()))
-
-    def draw(self):
-        if not self.time_count:
-            return
-
-
-class ActionManager:
-    def handle(self, event):
-        pass
+game = GameController()
 
 
 def main():
-    net_man = NetworkManager()
-    screen_man = ScreenManager()
-    player = Player()
-
-    action_man = ActionManager()
-
     clock = pygame.time.Clock()
 
     running = True
@@ -48,11 +21,11 @@ def main():
                 pygame.quit()
                 break
             elif event.type == pygame.MOUSEBUTTONUP:
-                net_man.send(str.encode(player.get_time_count))
+                game.net_man.send(str.encode(game.player.get_time_count))
 
         if running:
-            screen_man.window.fill((0, 0, 0))
-            screen_man.redraw()
+            game.action_man.handle()
+            game.screen_man.redraw()
             pygame.display.flip()
 
 
--- sprites.py
+++ sprites.py
@@ -1,17 +1,55 @@
 import pygame
 
 from const import TILE_SIDE_PX
+from network import NetworkManager
+from player import Player, ActionManager
+
+WHITE = (255, 255, 255)
 
 
 class SpriteFactory:
     @staticmethod
     def get_scaled_image(image, k):
-        loaded = pygame.image.load(image)
-        size = loaded.get_size()
-        return pygame.transform.scale(loaded, (int(size[0] * k), int(size[1] * k)))
+        image = pygame.image.load(image)
+
+        if image.get_alpha():
+            image = image.convert_alpha()
+        else:
+            image = image.convert()
+            image.set_colorkey(WHITE)
+
+        size = image.get_size()
+        return pygame.transform.scale(image, (int(size[0] * k), int(size[1] * k)))
 
     def create(self, image, x, y):
         return CommonSprite(self.group, image, x, y)
+
+
+class UIFactory(SpriteFactory):
+    def run(self, group, focus_group):
+        map_frame = CommonSprite(group, pygame.image.load('sprites/map_frame.jpg'), 32, 32)
+        info_frame = CommonSprite(group, pygame.image.load('sprites/info_frame.jpg'), 576, 32)
+        action_frame = CommonSprite(group, pygame.image.load('sprites/action_frame.jpg'), 576, 448)
+        player = PlayerFactory(32, 32).run(group)
+        # TODO: не место здесь
+        focus_image = CommonSprite(focus_group, self.get_scaled_image('sprites/tile_focus.png', 2), 16, 16)
+        result = {
+            'map_frame': map_frame,
+            'player': player,
+            'info_frame': info_frame,
+            'action_frame': action_frame,
+            'focus_image': focus_image,
+        }
+        return result
+
+
+class PlayerFactory(SpriteFactory):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def run(self, group):
+        return CommonSprite(group, self.get_scaled_image('sprites/player.png', 2), self.x, self.y)
 
 
 class MapTilesFactory(SpriteFactory):
@@ -42,17 +80,26 @@ class MapTilesFactory(SpriteFactory):
         return tiles
 
 
+class GameController:
+    def __init__(self):
+        self.screen_man = ScreenManager()
+        self.net_man = NetworkManager()
+        self.screen_man = ScreenManager()
+        self.player = Player()
+        self.action_man = ActionManager(self)
+
+
 class ScreenManager:
     def __init__(self):
         self.window = pygame.display.set_mode((31 * TILE_SIDE_PX, 18 * TILE_SIDE_PX))
         self.sprite_group = pygame.sprite.Group()
+        self.focus_group = pygame.sprite.GroupSingle()
 
         pygame.display.set_caption('Client')
 
-        # controllers initiation
-        map_frame = CommonSprite(self.sprite_group, pygame.image.load('sprites/map_frame.jpg'), 32, 32)
-        info_frame = CommonSprite(self.sprite_group, pygame.image.load('sprites/info_frame.jpg'), 576, 32)
-        action_frame = CommonSprite(self.sprite_group, pygame.image.load('sprites/action_frame.jpg'), 576, 448)
+        ui = UIFactory().run(self.sprite_group, self.focus_group)
+
+        map_frame = ui['map_frame']
 
         self.map_scheme = [
             'MSFXXXXXXXXXXXXX',
@@ -74,10 +121,7 @@ class ScreenManager:
         ]
 
         self.tiles = MapTilesFactory(self.map_scheme, map_frame, self.sprite_group).run()
-
-        # player
-        image = self.get_scaled_image('sprites/player.png', 2)
-        CommonSprite(self.sprite_group, image, 32, 32)
+        self.focus_image = ui['focus_image']
 
     def get_tiles_as_list(self):
         tiles = []
@@ -85,18 +129,10 @@ class ScreenManager:
             tiles += [tile for tile in row]
         return tiles
 
-    def get_scaled_image(self, image, k):
-        loaded = pygame.image.load(image)
-        size = loaded.get_size()
-        return pygame.transform.scale(loaded, (int(size[0] * k), int(size[1] * k)))
-
     def redraw(self):
         self.sprite_group.update()
         self.sprite_group.draw(self.window)
-
-        for tile in self.get_tiles_as_list():
-            if tile.rect.collidepoint(pygame.mouse.get_pos()):
-                self.window.blit(self.get_scaled_image('sprites/tile_focus.jpg', 2), tile.rect)
+        self.focus_group.draw(self.window)
 
 
 class CommonSprite(pygame.sprite.Sprite):
